a9965abbf7a2759cfab758d7ef595edcede4e4a3
jberella <berellajuancruz@hotmail.com>
2024-09-11 20:49:08 -0300

    se corrige nombre de de bucket para grupo13

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose222"
+    bucket                 = "pin-2-gh-terraform-aws-group13-tfstates"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
   }
 }
-
